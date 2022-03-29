9e5e446ab3f46736174845c34733ea604f6daee6
Bodesz03 <bodo.peter@students.jedlik.eu>
2022-03-29 11:34:27 +0200

    Progressing

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # VueJsFrontendMinta
+
+npm i -g @vue/cli
+
+vue create {project-name}
+
+npm i --save vue-router
+
---
+++ hahu-app/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+import OpenPageComponent from "components/OpenPage.vue";
+import IngatlanKinalatComponent from "components/IngatlanKinalat.vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+
+const router = createRouter({
+  history: createWebHistory,
+  routes: [
+    { path: "/", component: OpenPageComponent },
+    { path: "/offers", IngatlanKinalatComponent },
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+
+app.mount("#app");
